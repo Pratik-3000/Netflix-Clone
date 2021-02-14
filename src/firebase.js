b9a3c9bf1847ce0907c094403ccfd7a65add3e7b
Pratik <amonkar3000@example.com>
2021-02-14 12:04:35 +0530

    adding frontend to project

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB1QlG1U-FWirdrf822wZLaQesyfECGqpw",
+  authDomain: "netflix-clone-17cd8.firebaseapp.com",
+  projectId: "netflix-clone-17cd8",
+  storageBucket: "netflix-clone-17cd8.appspot.com",
+  messagingSenderId: "314533869079",
+  appId: "1:314533869079:web:2e813623b9b5707d494e52",
+  measurementId: "G-YZK1PKNHMC"
+};
